be49b67316d37fa311531d7e503605ea5fadd916
Andrew Read <aj.read13@gmail.com>
2022-01-19 10:16:21 -0500

    added beginnings of cli prompts

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const { randomUUID } = require("crypto");
 
-const inquirer = require("inquire"),
+const inquirer = require("inquire"),;
+const { listenerCount } = require("process");
 
+const options = [{
+    type: "list",
+    message: "Please select one of the below options.",
+    choices: [
+        "View All Employees.",
+        "View our Employees by department.",
+        "View all employee roles.",
+        "Update a current employee.",
+        "Add a New Employee.",
+        "Add a New Role.",
+        "Add New Department."
+    ]
+}];
+
+function userPrompt() {
+    inquirer.prompt(options) {
+        //switch case through options
+    }
+}
